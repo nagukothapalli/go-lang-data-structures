f32f8a12b8e2e71f8470299aa172c962c8fc8e74
nagukothapalli <nagukothapalli9@gmail.com>
2016-08-29 20:23:51 +0530

    Added cooment line arguments for stack capacity

--- src/stack-examples/StackTest.go
+++ src/stack-examples/StackTest.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"stack"
+	"strconv"
 )
 
 func main() {
 
+	// get the stack capacity
+	capacity := getStackCapacity(os.Args)
 	// get the  Integer Stack
 
-	arrayStack := stack.IStack(stack.GetnewStack(5))
+	arrayStack := stack.IStack(stack.GetnewStack(capacity))
 	// Testing Size
 	fmt.Printf("Stack Size %d : \n ", arrayStack.Size())
 	// using of POP opeerations
-	for i := 1; i <= 5; i++ {
+	for i := 1; i <= capacity; i++ {
 		anyError := arrayStack.Push(i * 25)
 		fmt.Printf("Element pushed: %d \n  ", i*25)
 		if anyError != nil {
@@ -23,7 +27,7 @@ func main() {
 	// Testing Size O(1)
 	fmt.Printf("Stack Size %d :  \n", arrayStack.Size())
 	// Usage of POP operation
-	for i := 0; i < 5; i++ {
+	for i := 0; i < capacity; i++ {
 		popElement, anyError := arrayStack.Pop()
 		if anyError == nil {
 			fmt.Printf("Element Popped : %d \n  ", popElement)
@@ -35,7 +39,7 @@ func main() {
 
 	// get the  Integer Stack
 
-	stringStack := stack.IStack(stack.GetnewStack(5)) // checking againest the interface
+	stringStack := stack.IStack(stack.GetnewStack(capacity)) // checking againest the interface
 	// Testing Size
 	fmt.Printf("Stack Size %d : \n ", stringStack.Size())
 	// using of POP opeerations
@@ -52,7 +56,7 @@ func main() {
 	// Testing Size O(1)
 	fmt.Printf("Stack Size %d :  \n", stringStack.Size())
 	// Usage of POP operation
-	for i := 0; i < 5; i++ {
+	for i := 0; i < capacity; i++ {
 		value, anyError := stringStack.Pop()
 		if anyError == nil {
 			fmt.Printf("Element Popped : %s \n  ", value)
@@ -61,4 +65,17 @@ func main() {
 		}
 	}
 	fmt.Printf("Stack Size : %d  \n ", stringStack.Size())
+
+}
+
+func getStackCapacity(args []string) int {
+	if len(args) != 2 {
+
+		fmt.Println("Usage of the Programme is incorrect Usage: <Program.go> StackCapacity_In_Number")
+		os.Exit(1)
+
+	}
+	value, _ := strconv.Atoi(args[1])
+	return value
+
 }
